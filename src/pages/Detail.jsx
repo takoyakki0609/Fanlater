a20120dc9a7167eb938df6ffdeb90e7284e7b3f6
takoyakki0609 <kjy6478@naver.com>
2023-11-15 20:48:18 +0900

    버튼수정

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,43 +1,102 @@
-import React from "react";
-import { useNavigate, useParams,useLocation } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate, useParams, useLocation } from "react-router-dom";
+import styled from "styled-components";
 
+const FanlaterBox = styled.section`
+  display: flex;
+  flex-direction: column;
+  background-color: black;
+  color: white;
+  padding: 30px 0;
+  width: 700px;
+  min-height: 500px;
+`;
 
 const Detail = () => {
+  const [edit, setEdit] = useState(false); // 수정에 관한 state
   const location = useLocation();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const params = useParams();
-  console.log(params)
+  console.log(params);
   console.log(location);
 
   const foundData = location.state.find((item) => {
     return item.id == params.id;
   });
 
-  console.log(foundData);
+  const [editContent, setEditContent] = useState(foundData.content);
+  const onClickDelete = (id) => {
+    //삭제 클릭시 삭제하겠냐는 alret띄움, 삭제 뒤 홈으로 이동
+    let answer = window.confirm("삭제하시겠습니까?");
+    if (answer === true) {
+      alert("삭제되었습니다.");
+    }
+    window.location.href="/"
+  };
+
   return (
-    <div>
-      <button onClick={()=>{
-        navigate("/")
-      }}>홈으로 </button>
-    <section>
-        <div>
-          <figure>
-            <img src={foundData.avatar} alt="유저아바타" />
-          </figure>
-          <div>
-            <h2>{foundData.nickname}</h2>
+    <div style={{ maxWidth: "1200px", margin: "0 auto" }}>
+      <button
+        style={{ marginTop: "40px", marginBottom: "20px" }}
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        홈으로{" "}
+      </button>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <FanlaterBox>
+          <div style={{ display: "flex", alignItems: "center", justifyContent:'space-around' }}>
+            <figure>
+              <img
+                src={foundData.avatar}
+                alt="유저아바타"
+                style={{ width: "100px", borderRadius: "50%" }}
+              />
+            </figure>
+
+            <h2 style={{ fontSize: "38px" }}>{foundData.nickname}</h2>
             <p>{foundData.createdAt}</p>
           </div>
-          <p>To. {foundData.writedTo}</p>
-          <div>
-            <p>{foundData.content}</p>
+          <p style={{ margin: "20px 10px" }}>To. {foundData.writedTo}</p>
+          <div style={{ margin: "0 auto" }}>
+            {edit === true ? (
+              <textarea
+                style={{ width: "600px", height: "300px", fontSize: "20px" }}
+                value={editContent}
+                onChange={(e) => {
+                  setEditContent(e.target.value);
+                }}
+              ></textarea>
+            ) : (
+              <p>{editContent}</p>
+            )}
           </div>
-        </div>
-        <div>
-          <button>수정</button>
-          <button>삭제</button>
-        </div>
-      </section>
+
+          {edit ? (
+            <div>
+              <button
+                onClick={() => {
+                  setEdit(false);
+                }}
+              >
+                수정완료
+              </button>
+            </div>
+          ) : (
+            <div>
+              <button
+                onClick={() => {
+                  setEdit(true);
+                }}
+              >
+                수정
+              </button>
+              <button onClick={() => onClickDelete(foundData.id)}>삭제</button>
+            </div>
+          )}
+        </FanlaterBox>
+      </div>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,27 +3,33 @@ import styled from "styled-components";
 import dummy from "../shared/fakeData.json";
 import { Link, useNavigate } from "react-router-dom";
 import uuid from "react-uuid";
-
+import { isVisible } from "@testing-library/user-event/dist/utils";
 
 //버튼스타일
 const style = {
-  "border": "none",
-  "padding": '10px',
-  "margin":'10px',
-  "cursor":"pointer",
-  "background":"white"
-}
+  border: "none",
+  padding: "10px",
+  margin: "10px",
+  cursor: "pointer",
+  background: !isVisible ? "yellow" : "transparent",
+};
 
 const Container = styled.div`
   margin: 0 auto;
   max-width: 1200px;
   text-align: center;
-  background: url(../assets/nongdamgom.png);
 `;
-
+const TitleBox = styled.div`
+  background: url("https://github.com/takoyakki0609/Fanlater/blob/main/src/assets/nongdamgom.png?raw=true");
+  background-size: cover;
+  width: 100%;
+  height: 500px;
+`;
 const Title = styled.h1`
   font-size: 30px;
-  margin-top: 50px;
+  font-weight: bold;
+  text-shadow: -1px 0px white, 0px 1px white, 1px 0px white, 0px -1px white;
+  padding-top: 200px;
 `;
 
 const Form = styled.form`
@@ -59,7 +65,7 @@ const Users = styled.li`
   color: white;
   transition: all 0.5s;
   cursor: pointer;
-  &:hover{
+  &:hover {
     transform: scale(1.05);
   }
 `;
@@ -68,48 +74,76 @@ export default function Home() {
   const navigate = useNavigate();
 
   //항상 한번 바꾸면 하ㄴ번 테스트 하기
-  const [data, setData] = useState(dummy)
-  const [nickname, setNickname] = useState('')
-  const [content, setContent] =useState('')
-  const [seleted, setSeleted] = useState("")
+  const [data, setData] = useState(dummy);
+  const [viewData, setViewData] = useState(dummy);
+  const [color, setColor] = useState(false);
+  const [nickname, setNickname] = useState("");
+  const [content, setContent] = useState("");
+  const [seleted, setSeleted] = useState("");
 
   const enrollHandler = (e) => {
     e.preventDefault();
     const newArr = {
-      id:uuid(),
-      createdAt:new Date().toString(),
-      nickname:nickname,
-      avatar:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaMLq7qLjd3tJE_MxbQzSk5BGng5SXecU82AVzphYuloDHl-cVyTYOiLiGRwDF9jZ1Fig&usqp=CAU",
-      content:content,
-      writedTo:seleted
-    }
-    setData((prev)=>{return [...prev, newArr]})
-  }
+      id: uuid(),
+      createdAt: new Date().toString(),
+      nickname: nickname,
+      avatar:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaMLq7qLjd3tJE_MxbQzSk5BGng5SXecU82AVzphYuloDHl-cVyTYOiLiGRwDF9jZ1Fig&usqp=CAU",
+      content: content,
+      writedTo: seleted,
+    };
+    setData((prev) => {
+      return [...prev, newArr];
+    });
+  };
 
   const onChangeNickname = (e) => {
-    setNickname(e.target.value)
-  }
+    setNickname(e.target.value);
+  };
   const onChangeContent = (e) => {
-    setContent(e.target.value)
-  }
+    setContent(e.target.value);
+  };
   const onChangeSeleted = (e) => {
-    setSeleted(e.target.value)
-    console.log(onChangeSeleted)
-  }
- 
+    setSeleted(e.target.value);
+    console.log(onChangeSeleted);
+  };
 
+/*   const onClicknongdamgom = () => {
+    const newArr = data;
+    const viewArr = newArr.filter((item)=>item.writedTo === "농담곰")
+    setViewData(viewArr)
+
+  };
+  const onClickKoroke = () =>{
+    const newArr = data;
+    const viewArr = newArr.filter((item)=>item.writedTo === "두더지 고로케")
+    setViewData(viewArr)
+  } */
+  const onClickFilter = (name) => {
+    const newArr = data;
+    const viewArr = newArr.filter((item)=>item.writedTo === name)
+    setViewData(viewArr)
+    if(name === 'all'){
+      setViewData(newArr)
+    }
+  }
+  
 
   return (
     <Container>
-      <div>
-        <Title>농담곰 팬레터 콜렉션</Title>
+      <TitleBox>
+        <Title>농담곰 팬레터</Title>
         <div>
-            <button style={style}>전체보기</button>
-            <button style={style}>농담곰</button>
-            <button style={style}>두더지 고로케</button>
-            <button style={style}>퍼그씨</button>
+          {/* writedTo가 같으면 보여주고 아니면 안보여주기.. ? */}
+          <button style={style} onClick={()=>onClickFilter('all')}>전체보기</button>
+          <button style={style} onClick={()=>onClickFilter('농담곰')}>
+            농담곰
+          </button>
+
+          <button style={style} onClick={()=>onClickFilter('두더지 고로케')}>두더지 고로케</button>
+          <button style={style} onClick={()=>onClickFilter('퍼그씨')}>퍼그씨</button>
         </div>
-      </div>
+      </TitleBox>
 
       <Form onSubmit={enrollHandler}>
         {/* 닉네임 */}
@@ -118,7 +152,7 @@ export default function Home() {
           <input
             type="text"
             value={nickname}
-            onChange={(e)=>onChangeNickname(e)}
+            onChange={(e) => onChangeNickname(e)}
             placeholder="최대 20글자까지 작성할 수 있습니다"
             maxLength="20"
             style={{ width: "100%", padding: "5px 10px" }}
@@ -132,7 +166,7 @@ export default function Home() {
             placeholder="최대 100자까지만 작성할 수 있습니다."
             maxLength="100"
             value={content}
-            onChange={(e)=>onChangeContent(e)}
+            onChange={(e) => onChangeContent(e)}
             style={{
               width: "100%",
               height: "80px",
@@ -156,16 +190,18 @@ export default function Home() {
       </Form>
       <div>
         <UserList>
-          {data.map((item) => {
+          {viewData.map((item) => {
             return (
               <Users key={item.id}>
                 {/* <Link to={`/detail/${item.id}`} style={{textDecoration:'none',color:'white'}}> */}
-                <div onClick={()=>{
-                  //1.경로 2.인자
-                  navigate(`/detail/${item.id}`, { state: data })
-                }}>
+                <div
+                  onClick={() => {
+                    //1.경로 2.인자
+                    navigate(`/detail/${item.id}`, { state: viewData });
+                  }}
+                >
                   <section style={{ display: "flex", alignItems: "center" }}>
-                    <figure style={{width:'100px'}}>
+                    <figure style={{ width: "100px" }}>
                       <img
                         src={item.avatar}
                         alt="유저아바타"
